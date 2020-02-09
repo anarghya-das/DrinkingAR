c544c435d7435640a7349ddafd6efbfa188b5dc8
Anarghya <anarghya@buffalo.edu>
2020-02-09 01:33:32 -0500

    initial commit

---
+++ Assets/Scripts/TapToPlace.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class TapToPlace : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private GameObject spawnedObject;
+    private ARRaycastManager _arRaycastManager;
+    private Vector2 touchPosition;
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    public void Awake()
+    {
+        _arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        else
+        {
+            touchPosition = default;
+            return false;
+        }
+    }
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if (_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(objectToSpawn, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+            }
+        }
+    }
+}
